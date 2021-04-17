792b4cdc82b3a1c71fda495ec3f76967901fc886
gongwentao-del <2905400500@qq.com>
2021-04-17 09:13:46 +0800

    1

--- app/src/main/java/com/example/re03/MainActivity.java
+++ app/src/main/java/com/example/re03/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.re03;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +11,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.rev01);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,reV.class);
+                startActivity(intent);
+            }
+        });
     }
 }
